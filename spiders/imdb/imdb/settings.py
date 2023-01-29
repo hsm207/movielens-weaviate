42d581c10a5972a08534b6e13ca21453ec1330e0
hsm207 <hsm207@users.noreply.github.com>
2023-01-29 01:02:25 +0000

    Configure download delay

--- spiders/imdb/imdb/settings.py
+++ spiders/imdb/imdb/settings.py
@@ -28,14 +28,16 @@ FAKEUSERAGENT_PROVIDERS = [
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
+RANDOMIZE_DOWNLOAD_DELAY = False
+
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
